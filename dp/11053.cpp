f3cd190447dd73e77bbe56971687ab34e82b07ab
최성현 <schoe@choeseonghyeon-ui-MacBookAir.local>
2023-01-15 23:20:48 +0900

    etc..

--- dp/11053.cpp
+++ dp/11053.cpp
@@ -7,25 +7,25 @@
 #include <stack>
 #include <vector>
 using namespace std;
-bool arr[1001];
-vector<int> v;
-int n,val,cnt,tcnt;
+int arr[1000001];
+int c[1000001];
+int n,ans,cnt,tcnt;
 int main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
   cin >> n;
+  fill(c,c +n,1);
   for (int i =0; i <n; ++i)
+    cin >> arr[i];
+  for (int i =1; i < n; ++i)
   {
-    cin >> val;
-    v.push_back(val);
+    for (int j = i -1; j >= 0; --j)
+    {
+      if (arr[j] >= arr[i]) continue;
+      if (c[j] < c[i]) continue;
+      c[i] = c[j] + 1;
+    }
   }
-  sort(v.begin(), v.end());
-  for (int i =0; i < v.size(); ++i)
-  {
-    if (arr[v[i]]) continue;
-    cnt++;
-    arr[v[i]] = true;
-  }
-  cout << cnt;
+  cout << *max_element(c, c+ 1000000);
 }
