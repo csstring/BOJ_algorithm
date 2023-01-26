0090f18101728c2e6164196d9200827b060d2bf3
최성현 <schoe@choeseonghyeon-ui-MacBookAir.local>
2023-01-26 16:09:45 +0900

    greedy end

--- greedy/8980.cpp
+++ greedy/8980.cpp
@@ -7,9 +7,37 @@
 #include <stack>
 #include <vector>
 using namespace std;
+int n,c,m,s,e,w,ans;
+int truck[2001];
+vector<tuple<int,int,int>> origin;
 int main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
+  cin >> n >> c>> m;
+  for (int i =0; i <m; ++i){
+    cin >> s>>e>>w;
+    origin.push_back({e,s,w});
+  }
+  sort(origin.begin(), origin.end());
+  for (auto cur : origin)
+  {
+    tie(e,s,w) = cur;
+    bool flag = true;
+    for (int i = s; i <e; ++i)
+    {
+      w = min(w, c - truck[i]);
+      if (w == 0){
+        flag = false;
+        break;
+      }
+    }
+    if (flag){
+      ans += w;
+      for (int i =s; i <e; ++i){
+        truck[i] += w;
+      }
+    }
+  }
+  cout << ans;
 }
