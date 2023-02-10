0d7d7626cf82ebbe6cdc10bb871c39f6f8885632
최성현 <schoe@choeseonghyeon-ui-MacBookAir.local>
2023-02-10 22:17:13 +0900

    1208

--- binary_serch/1208.cpp
+++ binary_serch/1208.cpp
@@ -1,22 +1,44 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <tuple>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <vector>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int b[41];
-int n,st,en,val;
-int main()
+size_t ans;
+int n,s,midn,val;
+vector<int> a,b;
+void inita()
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n;
-  for (int i =1; i <=n; ++i){
+  for (int i = 1; i <= midn; ++i)
+  {
     cin >> val;
-    b[i] = b[i-1] + val;
+    int s = a.size();
+    for (int j =0; j < s; ++j)
+      a.push_back(a[j]+ val);
+    a.push_back(val);
   }
-  
+}
+void initb()
+{
+  for (int i = midn+1; i <= n; ++i)
+  {
+    cin >> val;
+    int s = b.size();
+    for (int j =0; j < s; ++j)
+      b.push_back(b[j]+ val);
+    b.push_back(val);
+  }
+}
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+  cin >> n >> s;
+  midn = n/2;
+  inita(); initb();
+  sort(b.begin(), b.end());
+  sort(a.begin(), a.end());
+  ans += (upper_bound(b.begin(), b.end(), s) - lower_bound(b.begin(), b.end(), s));
+  ans += (upper_bound(a.begin(), a.end(), s) - lower_bound(a.begin(), a.end(), s));
+  for (int i=0; i < a.size();++i)
+    ans += (upper_bound(b.begin(), b.end(), s-a[i]) - lower_bound(b.begin(), b.end(), s-a[i]));
+  cout << ans;
 }
