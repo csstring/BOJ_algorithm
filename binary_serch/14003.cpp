c1d5e56eed4fe14339c88114e97d411048430bc3
최성현 <schoe@choeseonghyeon-ui-MacBookAir.local>
2023-02-13 23:45:21 +0900

    14003 ing

--- binary_serch/14003.cpp
+++ binary_serch/14003.cpp
@@ -18,29 +18,37 @@ int main()
   v.push_back(val);
   ans.push_back(val);
   s.push_back(0);
-  P = 1;
   for (int i =1; i < n; ++i)
   {
     cin >> val;
     if (v.back() < val){
       v.push_back(val);
-      if (s.begin()+P == s.end() ||is_sorted(s.begin()+P, s.end())){
-        ans.assign(v.begin(), v.end());
-        P = ans.size();
+      s.push_back(i);
+      ans.push_back(val);
+      auto it = is_sorted_until(s.rbegin(), s.rend(), greater<int>());
+      int dis = distance(it,s.rend());
+      for (int j = dis; j <v.size(); ++j)
+      {
+        ans[j] = v[j];
       }
+        //P = ans.size()-1;
     } 
     else{
       auto it = lower_bound(v.begin(), v.end(), val);
       *it = val;
       int dis = distance(v.begin(), it);
       s[dis] = i;
-      if (dis < P) P = dis;
+      //if (dis < P) P = dis;
     } 
   }
   cout << ans.size()<< '\n';
   for (int i =0; i < ans.size(); ++i)
     cout << ans[i] << ' ';
-  cout << endl;
+    /*
+  cout << '\n';
   for (int i =0; i < v.size(); ++i)
     cout << v[i] << ' ';
+  cout << '\n';
+  for (int i =0; i < s.size(); ++i)
+    cout << s[i] << ' ';*/
 }
