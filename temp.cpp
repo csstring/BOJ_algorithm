d16bdd92f7de1f16c330fc6aac3ac3148c5c7d17
최성현 <schoe@choeseonghyeon-ui-MacBookAir.local>
2023-02-13 22:12:57 +0900

    etc

--- temp.cpp
+++ temp.cpp
@@ -1,15 +1,25 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <tuple>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <vector>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<utility>
+vector<pair<int,int>> v;
+int w,n,m,p;
+size_t ans;
 using namespace std;
-int main()
+bool ft_cmp(pair<int,int> a, pair<int,int> b)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
+	return a.first > b.first;
+}
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> w >>n;
+	for (int i =0; i <n; ++i)
+	{
+		cin >>m >>p;
+		v.push_back({p,m});
+	}
+	sort(v.begin(), v.end(),ft_cmp);
+	return 0;
 }
