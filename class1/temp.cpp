ee915129444fbace31b83d24d23d6d7be05c5391
최성현 <schoe@choeseonghyeon-ui-MacBookAir.local>
2023-02-15 22:16:31 +0900

    etc

--- class1/temp.cpp
+++ class1/temp.cpp
@@ -9,14 +9,26 @@
 using namespace std;
 int ans,n,total,val1,val2;
 char val;
-vector<int> arr;
+int arr[30];
 string str;
 int main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cout.precision(12);
-  double a,b;
-  cin >> a >> b;
-  cout << a/b;
+  cin >> str;
+  fill(arr,arr+26,-1);
+  for (int i=0; i <str.size(); ++i){
+    arr[toupper(str[i]) - 65]++;
+  }
+  for (int i=0; i < 26; ++i)
+  {
+    if (arr[i] == 0) continue;
+    if (arr[i] > ans) {
+      val1 = i;
+      ans = arr[i];
+    } 
+  }
+  sort(arr,arr+26,greater<int>());
+  if (arr[0] == arr[1]) cout << '?';
+  else cout << (char)(val1 + 65);
 }
--- temp.cpp
+++ temp.cpp
@@ -2,24 +2,11 @@
 #include<vector>
 #include<algorithm>
 #include<utility>
-vector<pair<int,int>> v;
-int w,n,m,p;
-size_t ans;
 using namespace std;
-bool ft_cmp(pair<int,int> a, pair<int,int> b)
-{
-	return a.first > b.first;
-}
-int main(int argc, char** argv)
+int n;
+int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> w >>n;
-	for (int i =0; i <n; ++i)
-	{
-		cin >>m >>p;
-		v.push_back({p,m});
-	}
-	sort(v.begin(), v.end(),ft_cmp);
-	return 0;
+	
 }
